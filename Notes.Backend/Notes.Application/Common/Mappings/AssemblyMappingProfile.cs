9fe4e1cfa1bdc03a2bfea6c5154408cdf6d959b2
marinesque <flamefiu@yandex.ru>
2024-02-19 09:53:55 +0200

    mapper add

--- Notes.Backend/Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
+++ Notes.Backend/Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Reflection;
 using AutoMapper;
 
@@ -9,7 +11,23 @@ namespace Notes.Application.Common.Mappings
         public AssemblyMappingProfile(Assembly assembly) =>
             ApplyMappingsFromAssembly(assembly);
         
+        //Класс будет применять маппинг с помощью метода
+        //Будет сканировать сборку и искать любые интерфейсы, которые реализуют интерфейт IMapWith
         private void ApplyMappingsFromAssembly(Assembly assembly)
-        {}
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                              i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                    .ToList();
+            
+            //Вызывает метод от наследованного типа или из интерфейса, если не реализует этот метод
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new[] { this });
+            }
+        }
     }
 }
